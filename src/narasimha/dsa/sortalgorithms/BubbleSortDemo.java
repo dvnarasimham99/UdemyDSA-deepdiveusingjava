69e18acd361372ee12cb5664aec579a96f65a62f
Narasimha.MurthyDV <nmurthydv@deloitte.com>
2025-08-11 02:22:35 +0530

    First Commit to test - access check to push to main branch directly

--- src/narasimha/dsa/sortalgorithms/BubbleSortDemo.java
+++ src/narasimha/dsa/sortalgorithms/BubbleSortDemo.java
@@ -19,7 +19,7 @@ public class BubbleSortDemo
         for(int i=0; i<intArray.length;i++){
             System.out.println(intArray[i]);
         }
-
+        //Using for each loop
         for (int j : intArray) {
             System.out.println(j);
         }
